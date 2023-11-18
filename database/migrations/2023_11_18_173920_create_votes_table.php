359dc034dbd05e308e41f6aca05747325654764e
Mostafizur Rahman <csemrm@gmail.com>
2023-11-18 10:16:16 -0800

    DB ready

--- database/migrations/2023_11_18_173920_create_votes_table.php
+++ database/migrations/2023_11_18_173920_create_votes_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
+            $table->foreignIdFor(\App\Models\Option::class)->constrained();
         });
     }
 
